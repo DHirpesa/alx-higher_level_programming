e2344ea504fa2f727eefb64612266ecc5ccb491e
DHirpesa <dagnehirpesa@gmail.com>
2022-11-28 15:59:59 +0000

    Lets try

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -14,11 +14,11 @@ listint_t *fast = list;
 if (!list)
 return (0);
 
-while (slow && fast && fast ->)
+while (slow && fast && fast ->next)
 {
 slow = slow->next;
 fast = fast->next->next;
-if(slow == fast)
+if (slow == fast)
 return (1);
 }
 return (0);
